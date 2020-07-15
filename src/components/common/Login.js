e13e7f6b1bef5bb59490211c0f3a7c6ddfc86cf6
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2020-07-15 17:09:50 +0600

    context & hooks working

--- src/components/common/Login.js
+++ src/components/common/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import {
     Text,
     TextInput,
@@ -12,45 +12,42 @@ import {
 import { AuthContext } from '../../contexts/AuthContext';
 
 const Login = () => {
-    const { username, password, isAuthenticated, token } = useContext(AuthContext);
+    const { authCheck } = useContext(AuthContext);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        authCheck(username, password);
+        setUsername('');
+        setPassword('');
+    }
     return(
         <KeyboardAvoidingView style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
-            source={{
-              uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/AIUB_whole_logo.png/160px-AIUB_whole_logo.png',
-            }}
-          />
+          <Image style={styles.logo} source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/AIUB_whole_logo.png/160px-AIUB_whole_logo.png', }} />
           <Text style={styles.title}>
             American International University - Bangladesh
           </Text>
         </View>
         <View style={styles.formContainer}>
-          <TextInput
-            placeholder="Username"
+          <TextInput placeholder="Username"
             placeholderTextColor="rgba(255,255,255,0.7)"
             returnKeyType="next"
             keyboardType="email-address"
             autoCapitalize="none"
             autoCorrect={false}
             style={styles.textInput}
-            defaultValue={username.username}
-            // onChangeText={text => this.setState({username: text})}
-          />
-          <TextInput
-            placeholder="Password"
+            defaultValue={username}
+            onChangeText={text => setUsername(text)} />
+          <TextInput placeholder="Password"
             placeholderTextColor="rgba(255,255,255,0.7)"
             returnKeyType="go"
             secureTextEntry={true}
             style={styles.textInput}
-            defaultValue={password.password}
-            // onChangeText={text => this.setState({password: text})}
-          />
-          <TouchableOpacity style={styles.buttonContainer} 
-          // onPress={this.login}
-          >
+            defaultValue={password}
+            onChangeText={text => setPassword(text)} />
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleSubmit}>
             <Text style={styles.buttonText}>LOG IN</Text>
           </TouchableOpacity>
         </View>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -2,13 +2,20 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
-    const [username, setUsername] = useState({username: ''});
-    const [password, setPassword] = useState({password: ''});
-    const [isAuthenticated, setIsAuthenticated] = useState({isAuthenticated: false});
-    const [token, setToken] = useState({token: ''});
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [token, setToken] = useState('');
+
+    const authCheck = (username, password) => {
+        setUsername(username);
+        setPassword(password);
+        console.warn('username: ' + username);
+        console.warn('password: ' + password);
+    }
 
     return(
-        <AuthContext.Provider value={{username, password, isAuthenticated, token}}>
+        <AuthContext.Provider value={{username, password, isAuthenticated, token, authCheck}}>
             { props.children }
         </AuthContext.Provider>
     );
