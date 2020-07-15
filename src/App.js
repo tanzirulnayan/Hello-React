b88abbd6c10c5cecce885b9ceac541d94a739dfb
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2020-07-15 15:40:44 +0600

    login component created

--- src/App.js
+++ src/App.js
@@ -1,148 +1,18 @@
 import React, {Component} from 'react';
-import {
-  Text,
-  TextInput,
-  View,
-  Image,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  StatusBar,
-  StyleSheet,
-} from 'react-native';
+import { View } from 'react-native';
+import Login from './components/common/Login';
+import AuthContextProvider from './contexts/AuthContext';
 
 export default class App extends Component {
   constructor() {
     super();
-    // this.login = this.login.bind(this);
-    // this.state = {};
-    // this.setState();
   }
-
-  // login = () => {
-  //   if (this.state.username === 'nayan' && this.state.password === '1234') {
-  //     console.log('Login successful!');
-  //   } else if (
-  //     this.state.username !== 'nayan' &&
-  //     this.state.password !== '1234'
-  //   ) {
-  //     console.warn('Invalid username & password!');
-  //   } else if (
-  //     this.state.username !== 'nayan' &&
-  //     this.state.password === '1234'
-  //   ) {
-  //     console.warn('Invalid username!');
-  //   } else if (
-  //     this.state.username === 'nayan' &&
-  //     this.state.password !== '1234'
-  //   ) {
-  //     console.warn('Invalid password!');
-  //   } else {
-  //     console.warn('Invalid login credentials!');
-  //   }
-  // };
 
   render() {
     return (
-      <KeyboardAvoidingView style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
-            source={{
-              uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/AIUB_whole_logo.png/160px-AIUB_whole_logo.png',
-            }}
-          />
-          <Text style={styles.title}>
-            American International University - Bangladesh
-          </Text>
-        </View>
-        <View style={styles.formContainer}>
-          <TextInput
-            placeholder="Username"
-            placeholderTextColor="rgba(255,255,255,0.7)"
-            returnKeyType="next"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={styles.textInput}
-            // defaultValue={this.state.username}
-            // onChangeText={text => this.setState({username: text})}
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="rgba(255,255,255,0.7)"
-            returnKeyType="go"
-            secureTextEntry={true}
-            style={styles.textInput}
-            // defaultValue={this.state.password}
-            // onChangeText={text => this.setState({password: text})}
-          />
-          <TouchableOpacity style={styles.buttonContainer} 
-          // onPress={this.login}
-          >
-            <Text style={styles.buttonText}>LOG IN</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
+        <AuthContextProvider>
+          <Login></Login>
+        </AuthContextProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(94,92,230,1.0)',
-  },
-  logoContainer: {
-    alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  logo: {
-    height: 100,
-    width: 100,
-    marginBottom: 10,
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 16,
-    marginTop: 10,
-    width: 200,
-    textAlign: 'center',
-    opacity: 0.9,
-  },
-  buttonContainer: {
-    backgroundColor: 'rgba(88,86,214,1.0)',
-    paddingVertical: 15,
-    marginBottom: 20,
-    shadowColor: 'white',
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: '#FFFFFF',
-    fontWeight: '700',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#E91E63',
-  },
-  reactLogo: {
-    height: 50,
-    width: 50,
-    marginTop: 150,
-    marginBottom: 50,
-  },
-  textInput: {
-    height: 40,
-    width: 300,
-    backgroundColor: 'rgba(255,255,255,0.1)',
-    color: '#FFF',
-    borderWidth: 0,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-});
---
+++ src/components/common/Login.js
@@ -0,0 +1,120 @@
+import React, {useContext} from 'react';
+import {
+    Text,
+    TextInput,
+    View,
+    Image,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    StatusBar,
+    StyleSheet,
+  } from 'react-native';
+import { AuthContext } from '../../contexts/AuthContext';
+
+const Login = () => {
+    const { username, password, isAuthenticated, token } = useContext(AuthContext);
+    return(
+        <KeyboardAvoidingView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={{
+              uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/AIUB_whole_logo.png/160px-AIUB_whole_logo.png',
+            }}
+          />
+          <Text style={styles.title}>
+            American International University - Bangladesh
+          </Text>
+        </View>
+        <View style={styles.formContainer}>
+          <TextInput
+            placeholder="Username"
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            returnKeyType="next"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.textInput}
+            defaultValue={username.username}
+            // onChangeText={text => this.setState({username: text})}
+          />
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            returnKeyType="go"
+            secureTextEntry={true}
+            style={styles.textInput}
+            defaultValue={password.password}
+            // onChangeText={text => this.setState({password: text})}
+          />
+          <TouchableOpacity style={styles.buttonContainer} 
+          // onPress={this.login}
+          >
+            <Text style={styles.buttonText}>LOG IN</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(94,92,230,1.0)',
+    },
+    logoContainer: {
+      alignItems: 'center',
+      flexGrow: 1,
+      justifyContent: 'center',
+    },
+    logo: {
+      height: 100,
+      width: 100,
+      marginBottom: 10,
+    },
+    title: {
+      color: '#FFF',
+      fontSize: 16,
+      marginTop: 10,
+      width: 200,
+      textAlign: 'center',
+      opacity: 0.9,
+    },
+    buttonContainer: {
+      backgroundColor: 'rgba(88,86,214,1.0)',
+      paddingVertical: 15,
+      marginBottom: 20,
+      shadowColor: 'white',
+    },
+    buttonText: {
+      textAlign: 'center',
+      color: '#FFFFFF',
+      fontWeight: '700',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+      color: '#E91E63',
+    },
+    reactLogo: {
+      height: 50,
+      width: 50,
+      marginTop: 150,
+      marginBottom: 50,
+    },
+    textInput: {
+      height: 40,
+      width: 300,
+      backgroundColor: 'rgba(255,255,255,0.1)',
+      color: '#FFF',
+      borderWidth: 0,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+  });
+  
+export default Login;
